dc3afd2cf082406255c9826d560329986c7d145a
Xaver Hirsch <info@xaverhirsch.de>
2021-03-03 20:32:19 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { Suspense } from "react";
-import Bandcamp from "./components/Bandcamp";
+import React from "react";
 import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
-import Controls from "./components/Controls";
 
 function App() {
   return (
     <>
-      <Suspense fallback={null}>
-        <LandingPage />
-        {/* <Bandcamp /> */}
-      </Suspense>
+      <LandingPage />
       <Footer />
     </>
   );
